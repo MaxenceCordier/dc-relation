0e1811ea631b4ce020ce57e05cafc7efc8966a70
Pierre <houdantpierre@gmail.com>
2018-07-09 14:31:04 +0200

    Formulaire inscription

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'model/database.php';
 getHeader("Accueil");
 ?>
 
-        <form action="index.php" method="post" class="form-signin">
+        <form action="" method="post" class="form-signin">
             <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
             <label for="inputEmail" class="sr-only">Email</label>
             <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
@@ -15,11 +15,15 @@ getHeader("Accueil");
             <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
         </form>
 
-        <form action="index.php" method="post" class="">
+        <form action="create_user.php" method="post" class="">
             <h1 class="h3 mb-3 font-weight-normal">Inscription</h1>
             <label for="inputEmail" class="sr-only">Email</label>
             <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
-            <label for="inputPassword" class="sr-only">Mot de passe</label>
+            <select name="type" id="type">
+  <option value="volvo">Etudiant</option>
+  <option value="saab">Entreprise</option>
+</select>
+<label for="inputPassword" class="sr-only">Mot de passe</label>
             <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
         </form>
